c5f3922fe1b2b51b01ecc2797e15e1adad8fe6d8
kingrangE <yuvw123@gmail.com>
2025-07-11 21:01:56 +0900

    change the ruby version

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752235012',resources: [
+    cacheName: 'chirpy-1752235305',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
